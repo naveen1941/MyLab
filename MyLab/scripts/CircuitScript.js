f51850a00a6bef2356dd2465cb1808eb8ad5a458
naveen1941 <chittipolu@gmail.com>
2015-04-17 14:38:30 +0530

    Today 17. 3PM

--- MyLab/scripts/CircuitScript.js
+++ MyLab/scripts/CircuitScript.js
@@ -13,7 +13,9 @@ $(document).ready(function () {
     var canvasWidth=1000;
     var canvasHeight=600;
 
-    var unit=30;
+    var unit = 30;
+    var logical_rows = -1;
+    var logical_columns = -1;
 
     var paperDrawing = new joint.dia.Paper({
         el: $('#drawingPanel'),
@@ -21,7 +23,8 @@ $(document).ready(function () {
         height: canvasHeight,
         model: graphDrawing,
         gridSize: 1,
-        async: true 
+        async: true,
+        animation: true
     });
 
 
@@ -29,28 +32,33 @@ $(document).ready(function () {
     var addElementMIcon = function (x,y,imagepah,type) {
         var element = new joint.shapes.circuit.BaseElement({
             position: { x: x, y: y },
-            attrs: {
-                image: { 'xlink:href': '../images/'+imagepah }
-            }
+            //attrs: {
+            //    image: { 'xlink:href': '../images/'+imagepah }
+            //}
 
         });
         element.type = type;
         graphDrawing.addCell(element);
     }
 
-    var addPort = function (x,y) {
+    var addPort = function (x,y,x_log, y_log) {
         var element = new joint.shapes.circuit.BreadBoardPort({
             position: { x: x, y: y },
+            logicalPosition: { x: x_log, y: y_log },
+            isOccupied:false
         });
         graphDrawing.addCell(element);
     }
 
     var makeBreadBoard = function () {
-        for(var i=1;i<7;i++){
-            for (var j = 4 ; j <9; j++) {
-                addPort(i*unit,j*unit);
+        for(var i=1,k=0;i<10;i++,k++){
+            for (var j = 4,l=0 ; j <15; j++,l++) {
+                addPort(i*unit,j*unit, k,l);
             }
         }
+        logical_rows = k - 1;
+        logical_columns = l - 1;
+        console.log(logical_rows+':'+logical_columns);
     }
 
     makeBreadBoard();
@@ -58,15 +66,15 @@ $(document).ready(function () {
     var addElementTIcon = function (x, y, imagepah, type) {
         var element = new joint.shapes.circuit.BaseElement({
             position: { x: x, y: y },
-            attrs: {
-                image: { 'xlink:href': '../images/' + imagepah, 'ref-x': -10, 'ref-y': -10, width: 60 }
-            }
+            //attrs: {
+            //    image: { 'xlink:href': '../images/' + imagepah, 'ref-x': -10, 'ref-y': -10, width: 60 }
+            //}
         });
         element.type = type;
         graphDrawing.addCell(element);
     }
 
-    addElementMIcon(100, 30, 'image1.png', 'resistor');
+    addElementMIcon(10, 10, 'image1.png', 'resistor');
     addElementMIcon(250, 30, 'battery.png', 'battery');
     addElementTIcon(400, 50, 'bulbon.png', 'resistor');
   
@@ -75,30 +83,66 @@ $(document).ready(function () {
 
         if (cellView.model instanceof joint.shapes.circuit.BaseElement) {
 
-
+            var _pos_cellView = cellView.model.get('position');
+            var _referencePortBoundary = g.rect(_pos_cellView.x, _pos_cellView.y, unit, unit);
             var ports = graphDrawing.get('cells').find(function (cell) {
                 if (cell instanceof joint.shapes.circuit.BreadBoardPort) {
-                   
-                    if (cell.getBBox().containsPoint(g.point(x, y))) {
-                        console.log(cell);
+                    cell.attr({
+                        '.port': { fill: 'white' }
+                    });
+                    if (_referencePortBoundary.intersect(cell.getBBox())) {
+                        var log_pos=cell.get('logicalPosition');
+                        
+                        elementDrop(cellView, cell, function () {
+                            cellView.model.set('position', this._p0);
+                        });
+
                     }
                 }
             });
 
+
         }
-        
+        cellView.model.set('position', this._p0);
+    });
+
+    paperDrawing.on('cell:pointermove', function (cellView, evt, x, y) {
+
 
     });
 
+
+
+    var elementDrop = function (cellView,port,moveToInitial) {
+
+        var logicalPos = port.get('logicalPosition');
+
+        if(logicalPos.x+4>logical_rows || logicalPos.y+3>logical_columns || port.isOccupied){
+            moveToInitial();
+        }
+        else {
+            var portPosition = port.get('position');
+            var boundary = g.rect(portPosition.x, portPosition.y, 4 * unit, 3 * unit);
+
+            var ports = graphDrawing.findModelsInArea(boundary);
+            _.each(ports, function (each_port) {
+                each_port.isOccupied = true;
+                each_port.attr({
+                    '.port': { fill: 'black' }
+                });
+            });
+
+
+        }
+
+    }
+
+
+    
+
     paperDrawing.on('cell:pointerdown', function (cellView, evt, x, y) {
-
-        //console.log(cellView.model.getBBox());
-
-        
-            //console.log(cellView.model.get('position'));
-        //}
-
-        
+            
+        this._p0 = cellView.model.get('position');
 
     });
 
--- MyLab/scripts/joint.shapes.circuit.js
+++ MyLab/scripts/joint.shapes.circuit.js
@@ -15,10 +15,10 @@ joint.shapes.circuit = {};
 
 joint.shapes.circuit.BaseElement = joint.shapes.basic.Generic.extend({
 
-    markup: '<g class="rotatable"><g class="scalable"><rect class="body"/><image/></g><circle class="input"/><circle class="output"/><text class="text"/></g>',
+    markup: '<g class="rotatable element"><g class="scalable"><rect class="body"/><image/></g><circle class="input"/><circle class="output"/><text class="text"/></g>',
     defaults: joint.util.deepSupplement({
         type: 'circuit.BaseElement',
-        size: { width: 100, height: 60 },
+        size: { width: 120, height: 60 },
         attrs: {
             circle: { r: 7, stroke: 'black', fill: 'transparent', 'stroke-width': 2 },
             rect: { width: 80, height: 50 },
@@ -33,7 +33,8 @@ joint.shapes.circuit.BaseElement = joint.shapes.basic.Generic.extend({
                 width: 48, height: 48,
                 ref: '.body', 'y-alignment': 'middle', 'ref-y': 15
             },
-            '.body': { width: 50, height: 20, fill: 'transparent' },
+            '.element': { stroke: 'black', fill: 'transparent', 'stroke-width': 2 },
+            //'.body': { width: 50, height: 20, fill: 'transparent' },
             '.input': { r: 7, stroke: 'black', fill: 'transparent', 'stroke-width': 2 ,ref: '.body', 'ref-x': -2, 'ref-y': 0.5, magnet: 'passive', port: 'in' },
             '.output': { r: 7, stroke: 'black', fill: 'transparent', 'stroke-width': 2 , ref: '.body', 'ref-dx': 2, 'ref-y': 0.5, magnet: true, port: 'out' }
         }
